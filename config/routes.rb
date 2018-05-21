f3650c30c71aa3f8c94d5ef506fcadab4e8da609
Meredith Avery <quassibly@gmail.com>
2018-05-21 14:51:30 +0200

    with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,10 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'pets/index'
-  get 'pets/show'
-  get 'pets/new'
-  get 'pets/create'
-  get 'pets/edit'
-  get 'pets/update'
-  get 'pets/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pets do
+    resources :bookings, except: [:index]
+  end
+
+  resources :users, except: [:index, :destroy]
 end
