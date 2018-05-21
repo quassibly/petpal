36710abdf3e9baa69f6f124c57102ec775645797
Daniel Blake <daniel.blake1@hotmail.co.uk>
2018-05-21 16:18:14 +0200

    Controllers updated, seeds added without user_id

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,48 @@
 class BookingsController < ApplicationController
   def show
+    @user = current_user
+    @booking = Booking.find(@user)
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @user = current_user
+    @booking.user = @user
+    if @booking.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    if @booking.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_path(current_user)
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:pet_id, :start_date, :end_date)
+  end
+
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -41,6 +41,6 @@ class PetsController < ApplicationController
   private
 
   def pet_params
-    params.permit(:pet).require(:name, :type, :breed, :take_away, :home_stay, :age, :photo)
+    params.permit(:pet).require(:name, :animal_type, :breed, :take_away, :home_stay, :age, :photo)
   end
 end
