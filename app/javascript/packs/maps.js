ee52a98269de9e9533aa326433b32eeff5db4e51
Meredith Avery <quassibly@gmail.com>
2018-05-24 15:36:22 +0200

    map on booking info page.  API key still broken

--- app/javascript/packs/maps.js
+++ app/javascript/packs/maps.js
@@ -15,3 +15,17 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   }
 }
 
+const smallMapElement = document.getElementById('small-map');
+if (smallMapElement) {
+  const map = new GMaps({ el: '#small-map', lat: 52.507331, lng: 13.391398 });
+  const markers = JSON.parse(smallMapElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+}
