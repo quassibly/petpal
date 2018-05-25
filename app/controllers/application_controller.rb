206034ee5e1fc69c202344cd65a6d94e0c9ba757
Daniel Blake <daniel.blake1@hotmail.co.uk>
2018-05-25 12:00:17 +0200

    No longer need sign in before homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :home]
 
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
